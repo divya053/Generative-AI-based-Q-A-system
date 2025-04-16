10cfcbfc69e5cc6e7a9e8468abf02c3d9c0d566a
DIVYA   LALWANI <77092394+divya053@users.noreply.github.com>
2025-04-16 16:38:33 +0530

    Update download.py

--- download.py
+++ download.py
@@ -2,13 +2,13 @@ import requests
 
 def download_ggfu_file():
     file_id = "1A2B3C4D5E6F7G8H"  # replace with your real ID
-    url = f"https://drive.google.com/drive/folders/1K8qumxMXVZJ0P67nbDxJzqh-xTpxiCil?usp=drive_link"
+    url = f"https://drive.google.com/file/d/1v5_eMAlNWBW34ahL2zkH9n8kH0K-UUI1/view?usp=sharing"
     
     print("Downloading .ggfu file...")
     response = requests.get(url)
     
     if response.status_code == 200:
-        with open("data/file.ggfu", "wb") as f:
+        with open("models/hermes.ggfu", "wb") as f:
             f.write(response.content)
         print("File downloaded successfully!")
     else:
